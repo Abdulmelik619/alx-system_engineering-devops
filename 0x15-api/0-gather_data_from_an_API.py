ec00a5358ccf4259610a17e9a4df5a7c9aba7f0d
Abdulmelik619 <abdulmelikambw619@gmail.com>
2023-07-24 11:17:22 +0200

    api01py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,45 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+
 import requests
 import sys
 
+if len(sys.argv) != 2:
+    print("Usage: {} EMPLOYEE_ID".format(sys.argv[0]))
+    sys.exit(1)
 
-def tasks_done(id):
-    '''Script that displays an employee completed TODO tasks in stout
-        Parameters:
-        employee_id: Is an interger representing an employee id.
-    '''
+try:
+    employee_id = int(sys.argv[1])
+except ValueError:
+    print("EMPLOYEE_ID must be an integer")
+    sys.exit(1)
 
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    response = requests.get(url)
-    response_json = response.json()
-    employee_name = response_json.get("name")
+user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
 
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
-    todos = requests.get(url)
-    todos_json = todos.json()
-    number_tasks = len(todos_json)
+try:
+    user_response = requests.get(user_url)
+    user_response.raise_for_status()
+    user_data = user_response.json()
+    employee_name = user_data['name']
+except requests.exceptions.RequestException as e:
+    print("Error getting user data: {}".format(e))
+    sys.exit(1)
+except KeyError:
+    print("Error: Invalid user ID")
+    sys.exit(1)
 
-    task_compleated = 0
-    task_list = ""
+try:
+    todo_response = requests.get(todo_url)
+    todo_response.raise_for_status()
+    todo_data = todo_response.json()
+    total_tasks = len(todo_data)
+    done_tasks = [task for task in todo_data if task['completed']]
+    num_done_tasks = len(done_tasks)
+    task_titles = [task['title'] for task in done_tasks]
+except requests.exceptions.RequestException as e:
+    print("Error getting todo data: {}".format(e))
+    sys.exit(1)
 
-    for task in todos_json:
-        if task.get("completed") is True:
-            task_compleated += 1
-            task_list += "\t " + task.get("title") + "\n"
-
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-                                                          task_compleated,
-                                                          number_tasks))
-    print(task_list[:-1])
-
-
-if __name__ == "__main__":
-    tasks_done(sys.argv[1])
+print("Employee {} is done with tasks({}/{}):".format(employee_name, num_done_tasks, total_tasks))
+for title in task_titles:
+    print("\t {}".format(title))
